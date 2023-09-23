469aa81f7fcc089436a11423f77fea8db334965c
Jiwang Shao <sjww1997@126.com>
2023-09-23 11:33:52 +0800

    create project structure and write menu ui

---
+++ game/static/js/src/zbase.js
@@ -0,0 +1,14 @@
+class AcGame {
+    constructor(id) {
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.ac_game_menu = new AcGameMenu(this);
+        this.ac_game_playground = new AcGamePlayground(this);
+
+        this.start();
+    }
+
+    start() {
+
+    }
+}
--- game/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from game.views import index, other
-
-urlpatterns = [
-    path("", index, name="index"),
-    path("other/", other, name="other"),
-]
